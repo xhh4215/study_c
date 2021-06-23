354abd1c1baf591d504cd77dfe3c333d432f94c8
luanguiming <18734184215@163.com>
2021-06-23 15:18:22 +0800

    开始学习C语言的小例子

---
+++ cmake-build-debug/CMakeFiles/c_test_project.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c_test_project.dir/main.c.o"
+  "c_test_project"
+  "c_test_project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/c_test_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/***
+ * C语言学习的第一个例子
+ * @return
+ */
+int main() {
+  printf("Hello, World!\n");
+  return 0;
+}
